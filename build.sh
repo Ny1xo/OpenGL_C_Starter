c78303732bd96dce418462efafad0990e5d14b8b
Carson <86818471+Carssorole@users.noreply.github.com>
2025-05-08 17:22:18 -0500

    added missing linker
    GLEW wasn't initializing properly, discovered it requires direct linking, not just through the GL link, becuase we're linking libraries and not includes yadda-yadda

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
     #this script compiles the example OpenGL project
 	echo "helloWrold - This is an example of the start of an OpenGL project in c, using GLEW and GLFW"
 	#gcc -lglfw3 -lOpenGL -lX11 -lpthread -lXrandr -lXi -ldl src/main.c -o app
-	gcc src/example_main.c -lglfw -lGL -o App
+	gcc src/example_main.c -lglfw -lGL -lGLEW -o App
